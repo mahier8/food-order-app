a58d366e31421951a1254d7b14fefcae0d318629
Mahier Sydow <mahier.sydow8@gmail.com>
2021-06-08 18:27:18 +0900

    Context/Cart Reducer/Refs forward refs/Cart items/UseEffect

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import {useContext, useEffect, useState} from 'react';
 
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context'
 import classes from './HeaderCardButton.module.css'
 
 const HeaderCardButton = (props) => {
+    const [btnIsHighlighted, setButtonIsHighlighted] = useState(false);
+    const cartCtx = useContext(CartContext);
+
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((currNumber, item) => {
+        return currNumber + item.amount;
+    }, 0); 
+
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setButtonIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setButtonIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>
-            3
+            {numberOfCartItems}
         </span>
     </button>
     );
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import {useContext} from 'react';
+
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContext);
+
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+    };
 
     return (
         <li className={classes.meal}>
@@ -13,10 +26,13 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm id={props.id}/>
+                <MealItemForm onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
 };
 
 export default MealItem;
+
+// This was in the MealItemForm
+// id={props.id}
